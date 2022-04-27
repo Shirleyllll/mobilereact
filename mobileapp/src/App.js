5b08367f3df6dbe9947633e6822ddb3cf06f525e
liujingqi <765712426@qq.com>
2022-04-27 16:47:22 +0800

    配置项目路由

--- mobileapp/src/App.js
+++ mobileapp/src/App.js
@@ -1,14 +1,24 @@
+import { BrowserRouter as Router, Route, Switch, Redirect, Link } from 'react-router-dom'
+import React, { Suspense } from 'react'
+const Login = React.lazy(() => import('@/pages/Login'))
+const Home = React.lazy(() => import('@/pages/Home'))
 
-import { Button, Toast } from 'antd-mobile'
 const App = () => {
-    const test = () => {
-        Toast.success('Load success !!!', 1)
-    }
     return (
-        <div>
-            <Button type='primary' onClick={test}>Primary</Button>
-        </div>
-    );
+        <Router>
+            <div className="app">
+                {/* <Link to="/login">登录</Link>
+                <Link to="/home">首页</Link> */}
+                <Suspense fallback={<div>loading...</div>}>
+                    <Switch>
+                        <Redirect exact from="/" to="/home"></Redirect>
+                        <Route path="/login" component={Login}></Route>
+                        <Route path="/home" component={Home}></Route>
+                    </Switch>
+                </Suspense>
+            </div>
+        </Router>
+    )
 }
 
 export default App;
