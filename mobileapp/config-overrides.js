a834355cf4bbad76152be5f262aa2306671caea5
liujingqi <765712426@qq.com>
2022-04-27 14:24:47 +0800

    配置快捷路径

--- mobileapp/config-overrides.js
+++ mobileapp/config-overrides.js
@@ -1,9 +1,19 @@
 //用于覆盖webpack的配置
-const { override, fixBabelImports } = require('customize-cra')
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra')
+const path = require('path')
 
+//antd的按需加载
+const babelPlugin = fixBabelImports('import', {
+    libraryName: 'antd-mobile',
+    style: 'css',
+})
+
+//配置快捷路径别名
+const alias = addWebpackAlias({
+    '@': path.join(__dirname, 'src'),
+    '@scss': path.join(__dirname, 'src/assets/styles'),
+})
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd-mobile',
-        style: 'css',
-    })
+    babelPlugin,
+    alias
 )
--- mobileapp/src/index.js
+++ mobileapp/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 // import 'antd-mobile/dist/antd-mobile.css';
-import './assets/styles/index.scss'
+import '@scss/index.scss'
 ReactDOM.render(
     <App />
 ,document.getElementById('root')
